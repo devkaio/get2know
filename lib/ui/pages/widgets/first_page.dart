e6a4a50a7b6c49e2aab4969eaaa0f5edf1927ad5
Kaio C. de Oliveira <hello@kaio.dev>
2022-12-20 17:39:05 -0300

    style: update constraints

--- lib/ui/pages/widgets/first_page.dart
+++ lib/ui/pages/widgets/first_page.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
 class First extends StatelessWidget {
@@ -12,75 +13,33 @@ class First extends StatelessWidget {
     return Container(
       color: const Color(0xFF593493),
       child: Stack(
+        alignment: AlignmentDirectional.center,
         children: [
           Padding(
             padding: const EdgeInsets.all(100.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Quem sou eu?",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline1
-                          ?.apply(color: Colors.white),
-                    ),
-                    Text(
-                      "Kaio Costa de Oliveira",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline4
-                          ?.apply(color: Colors.white),
-                    ),
-                    Text(
-                      "Flutter Developer",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline4
-                          ?.apply(color: Colors.white),
-                    ),
-                    Text(
-                      "29 anos",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline4
-                          ?.apply(color: Colors.white),
-                    ),
-                    Text(
-                      "Casado",
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline4
-                          ?.apply(color: Colors.white),
+            child: LayoutBuilder(builder: (context, constraints) {
+              if (constraints.maxWidth > 1100.0) {
+                return ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 1500.0),
+                  child: Row(
+                    children: const [
+                      WhoAmIWidget(),
+                      Spacer(),
+                      ProfilePictureWidget(),
+                    ],
+                  ),
+                );
+              } else {
+                return Column(
+                  children: const [
+                    ProfilePictureWidget(),
+                    WhoAmIWidget(
+                      crossAxisAlignment: CrossAxisAlignment.center,
                     ),
                   ],
-                ),
-                const Spacer(),
-                Container(
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(175.0),
-                    ),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.all(
-                      Radius.circular(175.0),
-                    ),
-                    child: Image.network(
-                      'https://avatars.githubusercontent.com/u/3959419',
-                      fit: BoxFit.cover,
-                      width: 350,
-                      height: 350,
-                    ),
-                  ),
-                ),
-              ],
-            ),
+                );
+              }
+            }),
           ),
           Positioned(
             left: 0,
@@ -99,6 +58,80 @@ class First extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+class ProfilePictureWidget extends StatelessWidget {
+  const ProfilePictureWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(175.0),
+        ),
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(175.0),
+        ),
+        child: Image.network(
+          'https://avatars.githubusercontent.com/u/3959419',
+          fit: BoxFit.cover,
+          width: 350,
+          height: 350,
+        ),
+      ),
+    );
+  }
+}
+
+class WhoAmIWidget extends StatelessWidget {
+  final MainAxisAlignment mainAxisAlignment;
+  final CrossAxisAlignment crossAxisAlignment;
+  const WhoAmIWidget({
+    Key? key,
+    this.mainAxisAlignment = MainAxisAlignment.center,
+    this.crossAxisAlignment = CrossAxisAlignment.start,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: crossAxisAlignment,
+      children: [
+        Text(
+          "Quem sou eu?",
+          style:
+              Theme.of(context).textTheme.headline1?.apply(color: Colors.white),
+        ),
+        Text(
+          "Kaio Costa de Oliveira",
+          style:
+              Theme.of(context).textTheme.headline4?.apply(color: Colors.white),
+        ),
+        Text(
+          "Flutter Developer",
+          style:
+              Theme.of(context).textTheme.headline4?.apply(color: Colors.white),
+        ),
+        Text(
+          "29 anos",
+          style:
+              Theme.of(context).textTheme.headline4?.apply(color: Colors.white),
+        ),
+        Text(
+          "Casado",
+          style:
+              Theme.of(context).textTheme.headline4?.apply(color: Colors.white),
+        ),
+      ],
     );
   }
 }
